d26557bf1b04635af7d92681c5d2df4b1daf0f33
Greg Potts <gpotts@taos.com>
2015-06-13 16:45:39 -0700

    puppetdb::master::config not idempotent
    Removed now that puppetdb working so it doesn't restart puppet on every
    run.

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -6,5 +6,5 @@ class profiles::puppetmaster {
   # Configure puppetdb and its underlying database
   class { 'puppetdb': }
   # Configure the puppet master to use puppetdb
-  class { 'puppetdb::master::config': }
+  # class { 'puppetdb::master::config': }
 }
